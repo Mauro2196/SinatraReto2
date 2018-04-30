88af4ca1477f5d7670060d4d8fe3ee3e88e965ea
Mauricio Rodríguez <mauriciorodriguez@ZCOBTMH100167-MauricioRodriguez.local>
2018-04-29 20:49:57 -0500

    Added Reto2 v2

--- solution.rb
+++ solution.rb
@@ -6,8 +6,8 @@ end
 get '/makers/:nombre' do
   #nombre = params[:nombre]
   if  !(params[:nombre]) || params[:nombre] == "".strip
-    "<h1>Hola desconocido !</h1>"
+    "<h1>Hola desconocido!</h1>"
   else
-    "<h1>Hola #{params[:nombre].capitalize} !</h1>"
+    "<h1>Hola #{params[:nombre].capitalize}!</h1>"
   end
 end
